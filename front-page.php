1d38f3bbef91ade48580fe01728256a0fe48c8fa
Nicole Sedillo <ntsedillo@live.ca>
2024-03-21 13:04:46 -0700

    removed p tags from front page

--- front-page.php
+++ front-page.php
@@ -37,11 +37,11 @@ get_header();
         }
 
         if (get_field('about_restuarant_')) {
-            echo '<p>' . get_field('about_restuarant_') . '</p>';
+            the_field('about_restuarant_') ;
         }
         ?>
     </section>
-    
+
     <section class="location-section">
         <h2>Locations</h2>
         <?php
