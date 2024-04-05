349c392a79bb069b4ceb83d3b00898a40c3afb6c
genellepalana <genellepalana@gmail.com>
2024-04-04 17:21:06 -0700

    corrected typo

--- functions.php
+++ functions.php
@@ -355,7 +355,7 @@ add_filter('enter_title_here', 'vdx_change_testimonial_title', 10, 2);
 	 echo '<iframe width="100%" height="315" src="https://www.youtube.com/embed/oojxjpfcojw?si=KgETwG8RCU20nxth" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
  }
  
-//Second Outpust Function: Wordpress Turtorial Playlist
+//SECOND Widget Output Function: Navigating Wordpress- Detailed Guide
 function dashboard_widgets_display_2()
  {
 	 echo '<iframe width="100%" height="315" src="https://www.youtube.com/embed/FgZscfHWcXM?si=6Yd1QijxdwjDJxiS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
